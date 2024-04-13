dc881dc9d7cbb7259020c95dee7d12023d2a0944
hidemikata <h_mika22@bg8.so-net.ne.jp>
2024-04-13 14:15:22 +0900

    modified.

--- src/now_iso8601.ts
+++ src/now_iso8601.ts
@@ -1,5 +1,6 @@
 export function get_time(): string {
-    const currentTime: number = Math.floor(new Date().getTime() / 1000); // 現在時刻を秒単位で取得
+    const japanTimezoneOffset = 9 * 60 * 60 * 1000; // 9時間 * 60分 * 60秒 * 1000ミリ秒
+    const currentTime: number = Math.floor((new Date().getTime() + japanTimezoneOffset) / 1000); // 現在時刻を秒単位で取得
     const date = new Date(currentTime * 1000);
     const dateIsoString: string = date.toISOString();
     return dateIsoString;
